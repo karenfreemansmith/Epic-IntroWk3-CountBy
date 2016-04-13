eeadd21a58e12e21a32db6ec545bb7c2512f3951
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 21:37:25 -0700

    count by funtion displays correct numbers without error checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+//alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+$("#startCounting").click(function() {
+  var myNumbers=[];
+  var tn = parseInt($("#topNumber").val());
+  var cbn = parseInt($("#countByNumber").val());
+
+  for(i=0;i<=tn;i+=cbn) {
+    myNumbers.push(i);
+  }
+
+  $("#showCount").text(myNumbers.join(", "));
+});
